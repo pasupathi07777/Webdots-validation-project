632bb3c8b7f3c91feba18cf05c78544e3e7832b4
pasupathi07777 <pasupathi07777@gmail.com>
2025-03-27 16:51:52 +0530

    1.1

--- server/src/app.js
+++ server/src/app.js
@@ -9,12 +9,25 @@ dotenv.config({ path: "../.env" });
 
 
 const app=express()
+
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://webstone-assessment-project.onrender.com"
+];
+
 app.use(
-    cors({
-      origin: 'http://localhost:5173',
-      credentials: true, 
-    })
-  );
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, 
+  })
+);
 
 app.use(express.json({ limit: "1000mb" }));
 app.use(express.urlencoded({ limit: "1000mb", extended: true }));
