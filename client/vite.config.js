8d6529c47932fd7ee80ec3836fccf720c305031b
pasupathi07777 <pasupathi07777@gmail.com>
2025-03-27 17:09:21 +0530

    1.1

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [tailwindcss(),react()],
   build: {
-    outDir: "build",
+    outDir: "dist",
+    // outDir: "build",
   },
 })
